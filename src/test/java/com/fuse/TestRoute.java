5543a42458341ed1dfdd9ee0db05712ea8be9237
jorge castro <jorgecastro05@hotmail.com>
2018-07-15 15:57:35 -0500

    added cxf dependencies and testing classes

--- src/test/java/com/fuse/TestRoute.java
+++ src/test/java/com/fuse/TestRoute.java
@@ -1,4 +1,35 @@
 package com.fuse;
 
-public class TestRoute  {
+import org.apache.camel.builder.AdviceWithRouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.test.spring.CamelSpringTestSupport;
+import org.junit.Test;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestRoute extends CamelSpringTestSupport {
+
+    private String getMockEndpoint() {
+        return "mock:finish";
+    }
+
+    @Test
+    public void testRoute() throws Exception {
+        MockEndpoint mockEndpoint = getMockEndpoint(getMockEndpoint());
+        mockEndpoint.expectedMinimumMessageCount(1);
+        context.getRouteDefinition("cxf-to-queue").adviceWith(context, new AdviceWithRouteBuilder() {
+            @Override
+            public void configure() {
+                weaveAddLast().to(getMockEndpoint());
+            }
+        });
+
+        mockEndpoint.assertIsSatisfied();
+    }
+
+
+    @Override
+    protected AbstractApplicationContext createApplicationContext() {
+        return new ClassPathXmlApplicationContext("spring/camel-context.xml");
+    }
 }
